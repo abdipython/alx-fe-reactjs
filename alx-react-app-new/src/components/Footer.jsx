54dd352bfbeb0313d4fd019503906f2d5bcff81b
Abdulraman Saumu <abdulramansaumu@gmail.com>
2025-07-26 22:13:11 +0300

    debugging

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>&copy; 2025 My Travel Blog</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f9f9f9', minHeight: '300px' }}>
+      <p style={{ fontSize: '18px', color: '#333' }}>
+        Welcome to the list of my favorite cities around the world. Each city has a unique charm.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
