8937d6f29b7272abac62ffab3af72db6d6b0cb3b
Abdulraman Saumu <abdulramansaumu@gmail.com>
2025-07-27 13:08:43 +0300

    debuging

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -10,6 +10,31 @@ import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
+  return (
+    <Router>
+      <div>
+        <h1>🍲 Recipe Sharing App</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+function App() {
   const [count, setCount] = useState(0)
 
   return (
