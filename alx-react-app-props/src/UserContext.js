209fe526854e0ecac47e4569a63c83b7d0c03662
Abdulraman Saumu <abdulramansaumu@gmail.com>
2025-07-27 14:56:15 +0300

    done with task 2

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,18 @@
+// src/UserContext.js
+import { createContext } from 'react';
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
+const UserContext = createContext();
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+export default UserContext;
+
