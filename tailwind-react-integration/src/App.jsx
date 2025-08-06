038c6e48c3f090f973e1ea69277a694913731b99
Abdulraman Saumu <abdulramansaumu@gmail.com>
2025-08-06 21:53:47 +0300

    adding styling

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -33,5 +34,11 @@ function App() {
     </>
   )
 }
-
+function App() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <UserProfile />
+    </div>
+  );
+}
 export default App
